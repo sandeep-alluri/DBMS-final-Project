ca6a8f9204324fb00c8c249df468693abbe4013e
sandeep-alluri <89863764+sandeep-alluri@users.noreply.github.com>
2021-12-17 20:48:27 -0800

    Add files via upload

---
+++ DBCon.py
@@ -0,0 +1,30 @@
+import sqlite3  
+  
+cnt = sqlite3.connect("backup.dp")  
+  
+cnt.execute('''CREATE TABLE gfg(NAME TEXT,POINTS INTEGER,ACCURACY REAL);''')
+cnt.execute('''INSERT INTO gfg VALUES(
+'Count Inversion',20,80.5);''')
+cnt.execute('''INSERT INTO gfg(ACCURACY, POINTS, NAME) VALUES(
+90.5, 15, 'Kadanes Algo');''')
+cnt.execute('''INSERT INTO gfg(NAME, ACCURACY, POINTS) VALUES(
+'REVERSE STR', 100, 5);''')
+cnt.commit()
+print('Name, Points and Accuracy from '
+      'records with accuracy greater than 85')
+  
+cursor = cnt.execute('''SELECT * FROM gfg WHERE ACCURACY>85;''')
+for i in cursor:
+    print(i[0]+"    "+str(i[1])+"   "+str(i[2]))
+  
+print('')  # Print new line
+  
+print('Name, Accuracy from '
+      'records with accuracy greater than 85')
+  
+cursor = cnt.execute('''SELECT NAME, ACCURACY FROM
+gfg WHERE ACCURACY>85;''')
+
+for i in cursor:
+    print(i[0]+"    "+str(i[1]))
+
